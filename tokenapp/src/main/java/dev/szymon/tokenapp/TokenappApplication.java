449bc6ebdb98c6548d2ba3a9e2ee48df3c974bb7
Szymon <szymonswierz5@gmail.com>
2026-06-05 08:45:01 +0200

    Remove unused Feign configuration from tokenapp

--- tokenapp/src/main/java/dev/szymon/tokenapp/TokenappApplication.java
+++ tokenapp/src/main/java/dev/szymon/tokenapp/TokenappApplication.java
@@ -2,10 +2,8 @@ package dev.szymon.tokenapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class TokenappApplication {
 
 	public static void main(String[] args) {
